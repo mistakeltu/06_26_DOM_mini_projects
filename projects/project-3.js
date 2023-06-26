aaedefa7f801c1bd247f1788034601ebbe67e3f0
Karolis <karolisdarbinis2@gmail.com>
2023-06-26 20:37:15 +0300

    Mini Project 3

---
+++ projects/project-3.js
@@ -0,0 +1,7 @@
+const contentDOM = document.getElementsByClassName('content');
+
+for(let i = 0; i < contentDOM.length; i++){
+    contentDOM[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+    })
+};
