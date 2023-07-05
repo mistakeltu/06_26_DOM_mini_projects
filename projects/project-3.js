de3eeb138aaecca272f5540fc401bc9520533c6c
Karolis <karolisdarbinis2@gmail.com>
2023-07-05 07:30:36 +0300

    Update

--- projects/project-3.js
+++ projects/project-3.js
@@ -1,5 +1,6 @@
 const contentDOM = document.getElementsByClassName('content');
 
+
 for(let i = 0; i < contentDOM.length; i++){
     contentDOM[i].addEventListener('click', function(){
         this.classList.toggle('active');
